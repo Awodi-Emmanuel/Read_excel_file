c2b9d36ed7d0c6bf4184be4bb50c55dbb5ebc96d
NigerianCoder <gbemilanre@gmail.com>
2022-07-08 00:28:14 +0100

    tested celery task

--- core/read_excel.py
+++ core/read_excel.py
@@ -1,9 +1,17 @@
 from stringprep import in_table_a1
 import pandas as pd 
+from celery import shared_task
 
-f = pd.read_excel(r'store/account_details.xlsx',  usecols=['Numbers'])
-add_money = 50
+@shared_task
+def my_celery(request):
+    
+    f = pd.read_excel(r'store/account_details.xlsx',  usecols=['Numbers'])
+    add_money = 50
 
-for num in f.values:
-    print(num[0], '=', add_money)
-            
+    for num in f.values:
+        f.delay()
+        print(num[0], '=', add_money)
+    
+    return "Done" 
+
+                
